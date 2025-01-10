2a0d3acc99c73705182ef7d1e994018cbf6c09d6
kpak292 <konstantinpak89@gmail.com>
2025-01-10 11:30:58 +0500

    deleted unused imports

--- src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingMapper.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.booking.dto;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import ru.practicum.shareit.booking.entity.Booking;
